2ac82dfa275a6bad42e8ce13f24cd5e16c512e6a
Élaff Ramos <valelaff10@gmail.com>
2024-08-02 09:40:39 -0300

    Create inicio.js

---
+++ inicio.js
@@ -0,0 +1,4 @@
+let res = document.getElementById('res') //Chamando o resultado do html
+
+res.innerHTML = 'Login realizado com sucesso!<br>' //Isso é o que vai aparecer na página qunado o login for realizado com sucesso
+res.innerHTML += 'Aproveite o aplicativo e divirta-se.' //Isso é o que vai aparecer na página qunado o login for realizado com sucesso
